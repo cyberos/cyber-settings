f23baad73b9aac2942a1f1c06bd7b5e35fef7814
omame <me@omame.tech>
2020-12-10 17:24:18 +0100

    Background: base

---
+++ src/background.h
@@ -0,0 +1,25 @@
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+#include <QObject>
+#include <QList>
+#include <QVariant>
+
+class Background : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString currentBackgroundPath READ currentBackgroundPath WRITE setBackground NOTIFY backgroundChanged)
+    Q_PROPERTY(QList<QVariant> backgrounds READ backgrounds NOTIFY stub)
+
+public:
+    explicit Background(QObject *parent = nullptr);
+
+    QString currentBackgroundPath();
+    Q_INVOKABLE void setBackground(QString newBackgroundPath);
+
+signals:
+    void backgroundChanged();
+    void stub();
+}
+
+#endif
---
+++ src/qml/BackgroundPage.qml
@@ -0,0 +1,9 @@
+import QtQuick 2.0
+import QtQuick.Controls 2.5
+import QtQuick.Layouts 1.3
+import org.cyber.Settings 1.0
+import MeuiKit 1.0 as Meui
+
+ItemPage {
+    headerTitle: qsTr("Background")
+}
