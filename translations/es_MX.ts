9d40096ed1df2724ac31ebfea958ec09a1445a8e
Sebastian-byte <sebastianmontoya209@gmail.com>
2021-02-15 17:41:29 -0500

    Add Spanish translation

---
+++ translations/es_MX.ts
@@ -0,0 +1,457 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_MX" sourcelanguage="en_US">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="8"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="32"/>
+        <source>CyberOS</source>
+        <translation>CyberOS</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="42"/>
+        <source>OS Info</source>
+        <translation>Información del Sistema</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="49"/>
+        <source>Operating System</source>
+        <translation>Sistema Operativo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="54"/>
+        <source>Kernel Version</source>
+        <translation>Versión del Kernel</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="59"/>
+        <source>RAM Size</source>
+        <translation>Cantidad de Memoria RAM</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="64"/>
+        <source>Username</source>
+        <translation>Nombre de Usuario</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="69"/>
+        <source>Hostname</source>
+        <translation>Nombre del Equipo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="76"/>
+        <source>CyberOS Team</source>
+        <translation>Equipo CyberOS</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsPage</name>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="10"/>
+        <source>Accounts</source>
+        <translation>Cuentas</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="33"/>
+        <source>Currently logged in as</source>
+        <translation>Iniciado actualmente como</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="84"/>
+        <source>Hide additional settings</source>
+        <translation>Ocultar ajustes adicionales</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="84"/>
+        <source>Show additional settings</source>
+        <translation>Mostrar ajustes adicionales</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="94"/>
+        <source>Additional settings</source>
+        <translation>Ajustes adicionales</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="104"/>
+        <source>Automatic login</source>
+        <translation>Inicio de sesión automático</translation>
+    </message>
+</context>
+<context>
+    <name>AppearancePage</name>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="8"/>
+        <source>Appearance</source>
+        <translation>Apariencia</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="49"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="60"/>
+        <source>Light</source>
+        <translation>Claro</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="67"/>
+        <source>Dark</source>
+        <translation>Oscuro</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="82"/>
+        <source>Dim the wallpaper in dark theme</source>
+        <translation>Atenuar el fondo de pantalla en modo oscuro</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="101"/>
+        <source>Accent color</source>
+        <translation>Acentuar el color</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="154"/>
+        <source>Font</source>
+        <translation>Fuente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="166"/>
+        <source>General Font</source>
+        <translatorcomment>There are no better way of translating this</translatorcomment>
+        <translation>Fuente General</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="180"/>
+        <source>Fixed Font</source>
+        <translation>Fuente Fija</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="194"/>
+        <source>Font Size</source>
+        <translation>Tamaño de la Fuente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="203"/>
+        <source>Small</source>
+        <translation>Pequeña</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="207"/>
+        <source>Medium</source>
+        <translation>Mediana</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="211"/>
+        <source>Large</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="215"/>
+        <source>Huge</source>
+        <translation>Gigante</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundPage</name>
+    <message>
+        <location filename="../src/qml/BackgroundPage.qml" line="9"/>
+        <source>Wallpaper</source>
+        <translation>Fondo de pantalla</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryPage</name>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="8"/>
+        <source>Battery</source>
+        <translation>Batería</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="92"/>
+        <source>History</source>
+        <translation>Historial</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="226"/>
+        <location filename="../src/qml/BatteryPage.qml" line="228"/>
+        <source>Yesterday</source>
+        <translation>Ayer</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="234"/>
+        <location filename="../src/qml/BatteryPage.qml" line="236"/>
+        <source>Today</source>
+        <translation>Hoy</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="250"/>
+        <source>Last Charged to</source>
+        <translation>La última vez se cargo hasta</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="256"/>
+        <source>Maximum Capacity</source>
+        <translation>Capacidad Máxima</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="268"/>
+        <source>No battery found</source>
+        <translation>No se encontro ninguna batería</translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPage</name>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="8"/>
+        <source>Display</source>
+        <translation>Pantalla</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="35"/>
+        <source>Brightness</source>
+        <translation>Brillo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="82"/>
+        <source>Scale</source>
+        <translation>Escala</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="93"/>
+        <source>100%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="97"/>
+        <source>125%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="101"/>
+        <source>150%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="105"/>
+        <source>175%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="109"/>
+        <source>200%</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DockPage</name>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="8"/>
+        <source>Dock</source>
+        <translatorcomment>There are no direct translate</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="24"/>
+        <source>Position on screen</source>
+        <translation>Posición en pantalla</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="34"/>
+        <source>Left</source>
+        <translation>Izquierda</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="41"/>
+        <source>Bottom</source>
+        <translation>Abajo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="50"/>
+        <source>Size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="60"/>
+        <source>Small</source>
+        <translation>Pequeño</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="64"/>
+        <source>Medium</source>
+        <translation>Mediano</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="68"/>
+        <source>Large</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="72"/>
+        <source>Huge</source>
+        <translation>Gigante</translation>
+    </message>
+</context>
+<context>
+    <name>LanguagePage</name>
+    <message>
+        <location filename="../src/qml/LanguagePage.qml" line="8"/>
+        <source>Language</source>
+        <translation>Lenguaje</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkPage</name>
+    <message>
+        <location filename="../src/qml/NetworkPage.qml" line="11"/>
+        <source>Network</source>
+        <translation>Conexión a Internet</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NetworkPage.qml" line="40"/>
+        <source>Wired</source>
+        <translation>Conexión Cableada</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/NetworkPage.qml" line="80"/>
+        <source>Wi-Fi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="31"/>
+        <source>Accounts</source>
+        <translation>Cuentas</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="38"/>
+        <source>Display</source>
+        <translation>Pantalla</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="45"/>
+        <source>Network</source>
+        <translation>Conexión</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="52"/>
+        <source>Appearance</source>
+        <translation>Apariencia</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="59"/>
+        <source>Dock</source>
+        <translation>Dock</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="66"/>
+        <source>Wallpaper</source>
+        <translation>Fondo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="73"/>
+        <source>Language</source>
+        <translation>Lenguaje</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="80"/>
+        <source>Battery</source>
+        <translation>Batería</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="87"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="101"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessDetailsWindow</name>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="56"/>
+        <source>Auto-Join</source>
+        <translation>Auto Conexión</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="71"/>
+        <source>Security</source>
+        <translation>Seguridad</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="83"/>
+        <source>Signal</source>
+        <translation>Señal</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="95"/>
+        <source>IPv4 Address</source>
+        <translation>Dirección IPv4</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="107"/>
+        <source>IPv6 Address</source>
+        <translation>Dirección IPv6</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="119"/>
+        <source>MAC Address</source>
+        <translation>Dirección Física (MAC)</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="131"/>
+        <source>Gateway</source>
+        <translation>Puerta de Enlace Predeterminada</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="143"/>
+        <source>DNS</source>
+        <translation>Servidores DNS</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="159"/>
+        <source>Forget This Network</source>
+        <translation>Dejar de Recordar</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessItem</name>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="175"/>
+        <source>Password</source>
+        <translation>Contraseña</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="194"/>
+        <source>Show password</source>
+        <translation>Mostrar Contraseña</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="203"/>
+        <source>Connect</source>
+        <translation>Conectar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="211"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/qml/main.qml" line="12"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+</context>
+</TS>
