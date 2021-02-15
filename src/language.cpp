e596cb40e383803f4f2abdc6032b69d90522e923
omame <me@omame.tech>
2021-02-15 19:13:47 +0100

    Add Polish translation

--- src/language.cpp
+++ src/language.cpp
@@ -7,7 +7,7 @@
 #include <unicode/unistr.h>
 
 static const QStringList supportList = {"en_US", "en_AU", "en_CA", "en_NZ", "en_GB",
-                                        "ja", "ko", "ru", "de", "it", "zh_CN", "zh_HK"
+                                        "ja", "ko", "pl", "ru", "de", "it", "zh_CN", "zh_HK"
                                        };
 
 Language::Language(QObject *parent)
