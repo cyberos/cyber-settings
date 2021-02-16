71dc53e2ec2985b0937d7f0ea8c3525511ba04f0
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-02-16 17:32:17 +0300

    Add files via upload

---
+++ translations/ru_RU.ts
@@ -0,0 +1,445 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="8"/>
+        <source>About</source>
+        <translation>О системе</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="32"/>
+        <source>CyberOS</source>
+        <translation>CyberOS</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="42"/>
+        <source>OS Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="49"/>
+        <source>Operating System</source>
+        <translation>Операционная система</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="54"/>
+        <source>Kernel Version</source>
+        <translation>Версия ядра</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="59"/>
+        <source>RAM Size</source>
+        <translation>Размер оперативной памяти</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="64"/>
+        <source>Username</source>
+        <translation>Имя пользователя</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="69"/>
+        <source>Hostname</source>
+        <translation>Имя хоста</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="76"/>
+        <source>CyberOS Team</source>
+        <translation>Команда CyberOS</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsPage</name>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="10"/>
+        <source>Accounts</source>
+        <translation>Пользователи</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="33"/>
+        <source>Currently logged in as</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="84"/>
+        <source>Hide additional settings</source>
+        <translation>Скрыть дополнительные настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="84"/>
+        <source>Show additional settings</source>
+        <translation>Показать дополнительные настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="94"/>
+        <source>Additional settings</source>
+        <translation>Дополнительные настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AccountsPage.qml" line="104"/>
+        <source>Automatic login</source>
+        <translation>Автоматический вход</translation>
+    </message>
+</context>
+<context>
+    <name>AppearancePage</name>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="8"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="49"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="60"/>
+        <source>Light</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="67"/>
+        <source>Dark</source>
+        <translation>Темная</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="82"/>
+        <source>Dim the wallpaper in dark theme</source>
+        <translation>Затемнить обои в темной теме</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="101"/>
+        <source>Accent color</source>
+        <translation>Основной цвет</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="154"/>
+        <source>Font</source>
+        <translation>Шрифт</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="166"/>
+        <source>General Font</source>
+        <translation>Общий шрифт</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="180"/>
+        <source>Fixed Font</source>
+        <translation>Фиксированый шрифт</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="194"/>
+        <source>Font Size</source>
+        <translation>Размер шрифта</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="203"/>
+        <source>Small</source>
+        <translation>Маленький</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="207"/>
+        <source>Medium</source>
+        <translation>Средний</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="211"/>
+        <source>Large</source>
+        <translation>Большой</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AppearancePage.qml" line="215"/>
+        <source>Huge</source>
+        <translation>Огромный</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundPage</name>
+    <message>
+        <location filename="../src/qml/BackgroundPage.qml" line="9"/>
+        <source>Wallpaper</source>
+        <translation>Обон</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryPage</name>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="8"/>
+        <source>Battery</source>
+        <translation>Батарея</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="92"/>
+        <source>History</source>
+        <translation>История</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="226"/>
+        <location filename="../src/qml/BatteryPage.qml" line="228"/>
+        <source>Yesterday</source>
+        <translation>Вчера</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="234"/>
+        <location filename="../src/qml/BatteryPage.qml" line="236"/>
+        <source>Today</source>
+        <translation>Сегодня</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="250"/>
+        <source>Last Charged to</source>
+        <translation>Последний раз заряжен</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="256"/>
+        <source>Maximum Capacity</source>
+        <translation>Максимальная емкость</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/BatteryPage.qml" line="268"/>
+        <source>No battery found</source>
+        <translation>Батарея не найдена</translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPage</name>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="8"/>
+        <source>Display</source>
+        <translation>Экран</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="35"/>
+        <source>Brightness</source>
+        <translation>Яркость</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="82"/>
+        <source>Scale</source>
+        <translation>Масштаб</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="93"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="97"/>
+        <source>125%</source>
+        <translation>125%</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="101"/>
+        <source>150%</source>
+        <translation>150%</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="105"/>
+        <source>175%</source>
+        <translation>175%</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DisplayPage.qml" line="109"/>
+        <source>200%</source>
+        <translation>200%</translation>
+    </message>
+</context>
+<context>
+    <name>DockPage</name>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="8"/>
+        <source>Dock</source>
+        <translation>Док</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="24"/>
+        <source>Position on screen</source>
+        <translation>Позиция на экране</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="34"/>
+        <source>Left</source>
+        <translation>Слева</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="41"/>
+        <source>Bottom</source>
+        <translation>Внизу</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="50"/>
+        <source>Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="60"/>
+        <source>Small</source>
+        <translation>Маленький</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="64"/>
+        <source>Medium</source>
+        <translation>Большой</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="68"/>
+        <source>Large</source>
+        <translation>Большой</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/DockPage.qml" line="72"/>
+        <source>Huge</source>
+        <translation>Огромный</translation>
+    </message>
+</context>
+<context>
+    <name>LanguagePage</name>
+    <message>
+        <location filename="../src/qml/LanguagePage.qml" line="8"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkPage</name>
+    <message>
+        <location filename="../src/qml/NetworkPage.qml" line="11"/>
+        <source>Network</source>
+        <translation>Сеть</translation>
+    </message>
+</context>
+<context>
+    <name>SideBar</name>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="31"/>
+        <source>Accounts</source>
+        <translation>Пользователи</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="38"/>
+        <source>Display</source>
+        <translation>Экран</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="45"/>
+        <source>Network</source>
+        <translation>Сеть</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="52"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="59"/>
+        <source>Dock</source>
+        <translation>Док</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="66"/>
+        <source>Wallpaper</source>
+        <translation>Обои</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="73"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="80"/>
+        <source>Battery</source>
+        <translation>Батарея</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="87"/>
+        <source>About</source>
+        <translation>О системе</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SideBar.qml" line="101"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessDetailsWindow</name>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="56"/>
+        <source>Auto-Join</source>
+        <translation>Автоматическое подключение</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="71"/>
+        <source>Security</source>
+        <translation>Безопасность</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="83"/>
+        <source>Signal</source>
+        <translation>Сигнал</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="95"/>
+        <source>IPv4 Address</source>
+        <translation>IPv4 Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="107"/>
+        <source>IPv6 Address</source>
+        <translation>IPv6 Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="119"/>
+        <source>MAC Address</source>
+        <translation>MAC Адрес</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="131"/>
+        <source>Gateway</source>
+        <translation>Шлюз</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="143"/>
+        <source>DNS</source>
+        <translation>DNS</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessDetailsWindow.qml" line="159"/>
+        <source>Forget This Network</source>
+        <translation>Забыть эту сеть</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessItem</name>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="175"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="194"/>
+        <source>Show password</source>
+        <translation>Показать пароль</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="203"/>
+        <source>Connect</source>
+        <translation>Подключиться</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/WirelessItem.qml" line="211"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/qml/main.qml" line="12"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+</TS>
